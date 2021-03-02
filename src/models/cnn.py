3ec3c0ad9dc0b709dfcc1d1c26142aec7e329f4e
clementpiat <clement.piat2@gmail.com>
2021-03-02 09:00:50 +0100

    cnn: add pretrained_cnn class

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -28,3 +28,22 @@ class BaselineCNN(nn.Module):
         x = torch.flatten(x, start_dim=1)
         x = self.linear(x)
         return x
+
+class PretrainedCNN(nn.Module):
+    def __init__(self, size=16, cnn='vgg11'):
+        super(PretrainedCNN, self).__init__()
+        if cnn == 'vgg11':
+            self.net  = torch.hub.load('pytorch/vision:v0.6.0', 'vgg11', pretrained=True)
+            self.net .classifier[6] = nn.Linear(4096, size)
+        elif cnn == 'resnet18':
+            self.net = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
+            self.net.fc = nn.Linear(512, size)
+        else:
+            raise NameError('Invalid cnn name')
+
+    def forward(self, images):
+        """
+        images: a (n_images * 3 * image_size * image_size) tensor
+        returns: a (n_images * size) tensor
+        """
+        return self.net(images)
